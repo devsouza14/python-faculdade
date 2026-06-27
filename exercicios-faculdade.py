06a40398e0e3e86ba85e1cfc01ab9b1d35a1c875
devsouza14 <donsouza14@gmail.com>
2026-06-27 11:13:37 -0300

    Iniciando o repositório de python da faculdade

---
+++ exercicios-faculdade.py
@@ -0,0 +1,17 @@
+print("Hello, World!")
+
+lista = [10,11,12,13,14,15]
+for valor  in lista:
+  print(valor)
+
+for valor in range (5):
+  print(valor)
+
+for valor in range(5, 9):
+ print(valor)
+
+texto = input(str("Digite uma frase: "))
+letras = 0
+for letra in texto:
+  letras += 1 
+  print(letras)
