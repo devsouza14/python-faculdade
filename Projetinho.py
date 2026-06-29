e0abf98a61ce87017dd518ee9d997c8b8171342d
devsouza14 <donsouza14@gmail.com>
2026-06-28 21:42:10 -0300

    Parte da logica adicionada

--- Projetinho.py
+++ Projetinho.py
@@ -19,18 +19,30 @@ class carro:
     def infoDoVeiculo(Self):
         print(Self.marca, Self.modelo, Self.cor, Self.combustivel, Self.ano)
     
-    pass 
+    
 carro1 = carro('Gm','Celta','Branco', 'Flex', '2007')
 carro2 = carro('Volkswagen', 'Gol', 'Prata', 'Flex', '2015')
+carro3 = carro('Volkswagen', 'Jetta', 'Preto', 'Flex', '2016')
+
 
 opcao1 = carro1
 opcao2 = carro2
+opcao3 = carro3
 
-selecaoDeVeiculo = input("Selecione o veiculo: ")
-if selecaoDeVeiculo == opcao1:
-            print(carro1.infoDoVeiculo())
+selecaoDeVeiculo = input(str("Selecione o veiculo: "))
+
+if selecaoDeVeiculo == "opcao1":
+    opcao1.infoDoVeiculo()
+
+elif selecaoDeVeiculo == "opcao2":
+
+    opcao2.infoDoVeiculo()
+
 else:
-            print(carro2.infoDoVeiculo())
+
+    opcao3.infoDoVeiculo()
+      
+
 
 #carro2.Alugado()
 #carro1.Devolvido()
